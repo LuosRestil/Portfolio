7057dd2fd75b240f16e89426917cdcaf44136bc9
LuosRestil <metaltasteslikechicken@gmail.com>
2020-02-11 16:37:47 -0600

    Simplified responsive nav

--- index.js
+++ index.js
@@ -1,46 +1,58 @@
-$(document).ready(function() {
-  $(".nav-list li").click(function() {
-    $(this).addClass("selected");
-    $(".nav-list li")
-      .not(this)
-      .removeClass("selected");
-  });
-
-  $(window).scroll(() => {
-    $(".section").each(() => {
-      let sectionOffset = $(this.hash).offset().top;
-      if (sectionOffset <= $(this).scrollTop()) {
-        $(this).addClass("selected");
-      }
-    });
-  });
-
-  $(document).scroll(() => {
-    if ($(document).scrollTop > 95) {
-      $(".nav-wrapper").addClass("nav-beyond-landing");
-      $(".logo").addClass("hidden");
-    } else {
-      $(".nav-wrapper").removeClass("nav-beyond-landing");
-      $(".logo").removeClass("hidden");
-    }
-  });
-
+$(document).ready(() => {
+  // show/hide mobile side menu
   $(".menu-toggle").click(() => {
     $(".side-menu").addClass("show-menu");
-    $(".menu-toggle").addClass("no-opacity");
+    $(".menu-toggle").addClass("hide-toggle");
   });
   $(".x-wrapper").click(() => {
     $(".side-menu").removeClass("show-menu");
-    $(".menu-toggle").removeClass("no-opacity");
-  });
-
-  $(document).scroll(function() {
-    if ($(document).scrollTop() > 250) {
-      $(".mobilelogo").addClass("hidden");
-      $(".toggleMenu").css("padding", "9px 12px 9px 9px");
-    } else {
-      $(".mobilelogo").removeClass("hidden");
-      $(".toggleMenu").css("padding", "15px 20px 15px 15px");
-    }
+    $(".menu-toggle").removeClass("hide-toggle");
   });
 });
+
+// $(document).ready(function() {
+//   $(".nav-list li").click(function() {
+//     $(this).addClass("selected");
+//     $(".nav-list li")
+//       .not(this)
+//       .removeClass("selected");
+//   });
+
+//   $(window).scroll(() => {
+//     $(".section").each(() => {
+//       let sectionOffset = $(this.hash).offset().top;
+//       if (sectionOffset <= $(this).scrollTop()) {
+//         $(this).addClass("selected");
+//       }
+//     });
+//   });
+
+//   $(document).scroll(() => {
+//     if ($(document).scrollTop > 95) {
+//       $(".nav-wrapper").addClass("nav-beyond-landing");
+//       $(".logo").addClass("hidden");
+//     } else {
+//       $(".nav-wrapper").removeClass("nav-beyond-landing");
+//       $(".logo").removeClass("hidden");
+//     }
+//   });
+
+//   $(".menu-toggle").click(() => {
+//     $(".side-menu").addClass("show-menu");
+//     $(".menu-toggle").addClass("hide-toggle");
+//   });
+//   $(".x-wrapper").click(() => {
+//     $(".side-menu").removeClass("show-menu");
+//     $(".menu-toggle").removeClass("hide-toggle");
+//   });
+
+//   $(document).scroll(function() {
+//     if ($(document).scrollTop() > 250) {
+//       $(".mobilelogo").addClass("hidden");
+//       $(".toggleMenu").css("padding", "9px 12px 9px 9px");
+//     } else {
+//       $(".mobilelogo").removeClass("hidden");
+//       $(".toggleMenu").css("padding", "15px 20px 15px 15px");
+//     }
+//   });
+// });
